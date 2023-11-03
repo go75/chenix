5823277572465ffb6f995fe109953862c2257512
root <root@v>
2023-11-03 14:33:36 +0000

    update os

---
+++ os/src/lang_items.rs
@@ -0,0 +1,18 @@
+use core::panic::{PanicInfo, Location};
+use crate::{sbi::shutdown, println};
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    let msg = info.message().unwrap();
+    if let Some(location) = info.location() {
+        println!(
+            "Panicked at {}:{} {}",
+            location.file(),
+            location.line(),
+            msg
+        );
+    } else {
+        println!("Panicked: {}", msg);
+    }
+    shutdown()
+}
---
+++ os/src/main.rs
@@ -0,0 +1,29 @@
+#![no_main]
+#![no_std]
+mod lang_items;
+mod sbi;
+
+#[macro_use]
+mod console;
+
+use core::arch::global_asm;
+global_asm!(include_str!("entry.asm"));
+
+#![no_mangle]
+pub fn chenix_main() {
+    clean_bss();
+    println!("Hello chenix!");
+    panic!("Shutdown machine!");
+}
+
+fn clean_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe {
+            (a as *mut u8).write_volatile(0)
+        }
+    })
+}
